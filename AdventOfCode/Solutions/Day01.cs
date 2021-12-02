948cf210c4c1b2d624efc057d90f40ab8baff4af
Robin Krom <robin@getgreenshot.org>
2021-12-02 23:01:36 +0100

    Upgraded C# language version to 10 and used some features.

--- AdventOfCode/Solutions/Day01.cs
+++ AdventOfCode/Solutions/Day01.cs
@@ -1,58 +1,50 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using Xunit;
-using JM.LinqFaster;
+﻿namespace AdventOfCode.Solutions;
 
-namespace AdventOfCode.Solutions
+public class Day01 : AdventOfCodeBase
 {
-    public class Day01 : AdventOfCodeBase
+    private readonly Memory<int> _sonarDepths;
+
+    public Day01()
     {
-        private readonly Memory<int> _sonarDepts;
+        Assert.True(File.Exists(this.InputFilename));
+        _sonarDepths = File.ReadAllLines(this.InputFilename).Where(l => !string.IsNullOrEmpty(l)).Select(l => int.Parse(l)).ToArray();
+    }
 
-        public Day01()
+    public override string AnswerPartOne()
+    {
+        int? previousDept = null;
+        int increaseCount = 0;
+        foreach (var sonarDept in _sonarDepths.Span)
         {
-            Assert.True(File.Exists(this.InputFilename));
-            _sonarDepts = File.ReadAllLines(this.InputFilename).Where(l => !string.IsNullOrEmpty(l)).Select(l => int.Parse(l)).ToArray();
-        }
-
-        public override string AnswerPartOne()
-        {
-            int? previousDept = null;
-            int increaseCount = 0;
-            foreach (var sonarDept in _sonarDepts.Span)
+            if (sonarDept > previousDept)
             {
-                if (sonarDept > previousDept)
-                {
-                    increaseCount++;
-                }
-
-                previousDept = sonarDept;
-            }
-            return $"Answer 1: {increaseCount}";
-        }
-
-        public override string AnswerPartTwo()
-        {
-            int? previousDeptSum = null;
-            int increaseCount = 0;
-            for (var index = 0; index < _sonarDepts.Span.Length; index++)
-            {
-                int size = Math.Min(3, _sonarDepts.Span.Length - index);
-                if (size != 3) continue;
-                var window = _sonarDepts.Span.Slice(index, size);
-
-                var sum = window.SumF();
-                if (sum > previousDeptSum)
-                {
-                    increaseCount++;
-                }
-
-                previousDeptSum = sum;
+                increaseCount++;
             }
 
-            return $"Answer 2: {increaseCount}";
+            previousDept = sonarDept;
+        }
+        return $"Answer 1: {increaseCount}";
+    }
+
+    public override string AnswerPartTwo()
+    {
+        int? previousDeptSum = null;
+        int increaseCount = 0;
+        for (var index = 0; index < _sonarDepths.Span.Length; index++)
+        {
+            int size = Math.Min(3, _sonarDepths.Span.Length - index);
+            if (size != 3) continue;
+            var window = _sonarDepths.Span.Slice(index, size);
+
+            var sum = window.SumF();
+            if (sum > previousDeptSum)
+            {
+                increaseCount++;
+            }
+
+            previousDeptSum = sum;
         }
 
+        return $"Answer 2: {increaseCount}";
     }
 }
--- AdventOfCode/Solutions/Day02.cs
+++ AdventOfCode/Solutions/Day02.cs
@@ -1,75 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Xunit;
-using JM.LinqFaster;
+﻿namespace AdventOfCode.Solutions;
 
-namespace AdventOfCode.Solutions
+public class Day02 : AdventOfCodeBase
 {
-    public class Day02 : AdventOfCodeBase
+    private readonly List<string> _submarineCommands;
+
+    public Day02()
     {
-        private readonly List<string> _submarineCommands;
-
-        public Day02()
-        {
-            Assert.True(File.Exists(this.InputExampleFilename));
-            _submarineCommands = File.ReadAllLines(this.InputFilename).Where(l => !string.IsNullOrEmpty(l)).ToList();
-        }
-
-        public override string AnswerPartOne()
-        {
-            var position = 0;
-            var depth = 0;
-
-            foreach (var submarineCommand in _submarineCommands)
-            {
-                var commandInformation = submarineCommand.Split(" ");
-                var command = commandInformation[0];
-                var x = int.Parse(commandInformation[1]);
-                switch (command)
-                {
-                    case "forward":
-                        position += x;
-                        break;
-                    case "down":
-                        depth += x;
-                        break;
-                    case "up":
-                        depth -= x;
-                        break;
-                }
-            }
-            return $"Answer 1: {position*depth}";
-        }
-
-        public override string AnswerPartTwo()
-        {
-            var position = 0;
-            var depth = 0;
-            var aim = 0;
-            foreach (var submarineCommand in _submarineCommands)
-            {
-                var commandInformation = submarineCommand.Split(" ");
-                var command = commandInformation[0];
-                var x = int.Parse(commandInformation[1]);
-                switch (command)
-                {
-                    case "forward":
-                        position += x;
-                        depth += aim * x;
-                        break;
-                    case "down":
-                        aim += x;
-                        break;
-                    case "up":
-                        aim -= x;
-                        break;
-                }
-            }
-
-            return $"Answer 2: {position * depth}";
-        }
-
+        Assert.True(File.Exists(this.InputExampleFilename));
+        _submarineCommands = File.ReadAllLines(this.InputFilename).Where(l => !string.IsNullOrEmpty(l)).ToList();
     }
+
+    public override string AnswerPartOne()
+    {
+        var position = 0;
+        var depth = 0;
+
+        foreach (var submarineCommand in _submarineCommands)
+        {
+            var commandInformation = submarineCommand.Split(" ");
+            var command = commandInformation[0];
+            var x = int.Parse(commandInformation[1]);
+            switch (command)
+            {
+                case "forward":
+                    position += x;
+                    break;
+                case "down":
+                    depth += x;
+                    break;
+                case "up":
+                    depth -= x;
+                    break;
+            }
+        }
+        return $"Answer 1: {position*depth}";
+    }
+
+    public override string AnswerPartTwo()
+    {
+        var position = 0;
+        var depth = 0;
+        var aim = 0;
+        foreach (var submarineCommand in _submarineCommands)
+        {
+            var commandInformation = submarineCommand.Split(" ");
+            var command = commandInformation[0];
+            var x = int.Parse(commandInformation[1]);
+            switch (command)
+            {
+                case "forward":
+                    position += x;
+                    depth += aim * x;
+                    break;
+                case "down":
+                    aim += x;
+                    break;
+                case "up":
+                    aim -= x;
+                    break;
+            }
+        }
+
+        return $"Answer 2: {position * depth}";
+    }
+
 }
