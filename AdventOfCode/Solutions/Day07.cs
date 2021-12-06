17d78f5fc0b9efcee0446e5642b7fec1fdd9fdeb
Robin Krom <robin@getgreenshot.org>
2021-12-06 07:57:45 +0100

    Prepared for day07

---
+++ AdventOfCode/Solutions/Day07.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode.Solutions;
+
+public class Day07 : AdventOfCodeBase
+{
+    public Day07()
+    {
+        Initialize(this.InputFilename);
+    }
+
+    private void Initialize(string path)
+    {
+        Assert.True(File.Exists(path));
+        var input = File.ReadAllLines(path).First().Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse);
+    }
+
+    public override string AnswerPartOne()
+    {
+        long answer = 0;
+
+        return $"Answer 1: {answer}";
+    }
+}
