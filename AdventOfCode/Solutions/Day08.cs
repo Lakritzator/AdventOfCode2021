7f4f05855ba308a9c0d50605dd70da44815b0ede
Robin Krom <robin@getgreenshot.org>
2021-12-08 16:55:08 +0100

    Made the code for day 8 a bit more readable, and added an empty day 9.

--- AdventOfCode/Solutions/Day08.cs
+++ AdventOfCode/Solutions/Day08.cs
@@ -87,159 +87,25 @@ public class Day08 : AdventOfCodeBase
                     {
                         continue;
                     }
-                    switch (signalInformation.Length)
-                    {
-                        // Digit 1 has 2 segments
-                        case 2:
-                            knownSignals[1] = signalInformation;
-                            change = true;
-                            break;
-                        // Digit 7 has 3 segments
-                        case 3:
-                            knownSignals[7] = signalInformation;
-                            change = true;
-                            break;
-                        // Digit 4 has 4 segments
-                        case 4:
-                            knownSignals[4] = signalInformation;
-                            change = true;
-                            break;
-                        // Digit 8 has 7 segments
-                        case 7:
-                            knownSignals[8] = signalInformation;
-                            change = true;
-                            break;
-                        // Digits 2,3,5 all have 5 segments
-                        case 5:
-                            // If there are two similarities between 1 and our signal it's 3
-                            if (knownSignals[1] != null)
-                            {
-                                if (Union(knownSignals[1], signalInformation).Length == 2)
-                                {
-                                    knownSignals[3] = signalInformation;
-                                    change = true;
-                                    break;
-                                }
-                            }
-                            // If there are three similarities between 7 and our signal it's 3
-                            if (knownSignals[7] != null)
-                            {
-                                if (Union(knownSignals[7], signalInformation).Length == 3)
-                                {
-                                    knownSignals[3] = signalInformation;
-                                    change = true;
-                                    break;
-                                }
-                            }
-                            // If there are two similarities between 4 and our signal it's 2
-                            if (knownSignals[4] != null)
-                            {
-                                if (Union(knownSignals[4], signalInformation).Length == 2)
-                                {
-                                    knownSignals[2] = signalInformation;
-                                    change = true;
-                                    break;
-                                }
-                                if (Union(knownSignals[4], signalInformation).Length == 3)
-                                {
-                                    knownSignals[5] = signalInformation;
-                                    change = true;
-                                }
-                            }
-                            break;
-                        // Digits 0,6,9 all have 6 segments
-                        case 6:
-                            // If there is one similarity between 1 and our signal it's 6
-                            if (knownSignals[1] != null)
-                            {
-                                if (Union(knownSignals[1], signalInformation).Length == 1)
-                                {
-                                    knownSignals[6] = signalInformation;
-                                    change = true;
-                                    break;
-                                }
-                            }
-                            // If there are 2 similarities between 7 and our signal it's 6
-                            if (knownSignals[7] != null)
-                            {
-                                if (Union(knownSignals[7], signalInformation).Length == 2)
-                                {
-                                    knownSignals[6] = signalInformation;
-                                    change = true;
-                                    break;
-                                }
-                            }
-                            // If there are two similarities between 4 and our signal it's 2
-                            if (knownSignals[4] != null)
-                            {
-                                if (Union(knownSignals[4], signalInformation).Length == 4)
-                                {
-                                    knownSignals[9] = signalInformation;
-                                    change = true;
-                                    break;
-                                }
-                            }
 
-                            if (knownSignals[0] != null && knownSignals[6] != null)
-                            {
-                                knownSignals[9] = signalInformation;
-                                change = true;
-                                break;
-                            }
-                            if (knownSignals[0] != null && knownSignals[9] != null)
-                            {
-                                knownSignals[6] = signalInformation;
-                                change = true;
-                                break;
-                            }
-                            if (knownSignals[9] != null && knownSignals[6] != null)
-                            {
-                                knownSignals[0] = signalInformation;
-                                change = true;
-                            }
-                            break;
+                    if (DeductKnowSignal(knownSignals, signalInformation))
+                    {
+                        change = true;
                     }
+
                 }
             }
 
             Assert.DoesNotContain(null, knownSignals);
 
-            // Make mapping
-            var decodeMap = new char[7];
+            // Make mapping for decoding
+            var decodeMap = CreateDecodeMap(knownSignals);
 
-            // When you look what 7 has more than 1, you know what the encoded 'a' segment is
-            var aEncoded = Difference(knownSignals[7], knownSignals[1])[0];
-            decodeMap[(byte)aEncoded - (byte)'a'] = 'a';
-
-            // When you look what 9 has more than 3, you know what the encoded 'b' segment is
-            var bEncoded = Difference(knownSignals[9], knownSignals[3])[0];
-            decodeMap[(byte)bEncoded - (byte)'a'] = 'b';
-
-            // When you look what 8 has more than 6, you know what the encoded 'c' segment is
-            var cEncoded = Difference(knownSignals[8], knownSignals[6])[0];
-            decodeMap[(byte)cEncoded - (byte)'a'] = 'c';
-
-            // When you look what 8 has more than 0, you know what the encoded 'd' segment is
-            var dEncoded = Difference(knownSignals[8], knownSignals[0])[0];
-            decodeMap[(byte)dEncoded - (byte)'a'] = 'd';
-
-            // When you look what 8 has more than 9, you know what the encoded 'e' segment is
-            var eEncoded = Difference(knownSignals[8], knownSignals[9])[0];
-            decodeMap[(byte)eEncoded - (byte)'a'] = 'e';
-
-            // When you look what 8 (minus segment b) has more than 3, you know what the encoded 'f' segment is
-            var fEncoded = Difference(Remove(knownSignals[8], bEncoded), knownSignals[2])[0];
-            decodeMap[(byte)fEncoded - (byte)'a'] = 'f';
-
-            // When you look what 9 (minus segment a) has more than 4, you know what the encoded 'g' segment is
-            var gEncoded = Difference(Remove(knownSignals[9],aEncoded), knownSignals[4])[0];
-            decodeMap[(byte)gEncoded - (byte)'a'] = 'g';
-            
             // Map the encoded segment values, by decoding them, and matching them to existing segments
             StringBuilder builder = new();
             foreach (var segmentInformation in content.right)
             {
-                var decodedSegment = DecodePartTwo(segmentInformation, decodeMap);
+                var decodedSegment = DecodeSegments(segmentInformation, decodeMap);
                 builder.Append(_digitSegments.IndexOf(decodedSegment));
             }
             answer += int.Parse(builder.ToString());
@@ -248,12 +114,166 @@ public class Day08 : AdventOfCodeBase
     }
 
     /// <summary>
+    /// Take the signal information and try to map it using all the know signals so far
+    /// </summary>
+    /// <param name="knownSignals">string{}</param>
+    /// <param name="signalInformation">string</param>
+    /// <returns>boolean if something was found</returns>
+    private static bool DeductKnowSignal(string[] knownSignals, string signalInformation)
+    {
+        switch (signalInformation.Length)
+        {
+            // Digit 1 has 2 segments
+            case 2:
+                knownSignals[1] = signalInformation;
+                return  true;
+            // Digit 7 has 3 segments
+            case 3:
+                knownSignals[7] = signalInformation;
+                return true;
+            // Digit 4 has 4 segments
+            case 4:
+                knownSignals[4] = signalInformation;
+                return true;
+            // Digit 8 has 7 segments
+            case 7:
+                knownSignals[8] = signalInformation;
+                return true;
+            // Digits 2,3,5 all have 5 segments
+            case 5:
+                // If there are two similarities between 1 and our signal it's 3
+                if (knownSignals[1] != null)
+                {
+                    if (Union(knownSignals[1], signalInformation).Length == 2)
+                    {
+                        knownSignals[3] = signalInformation;
+                        return true;
+                    }
+                }
+                // If there are three similarities between 7 and our signal it's 3
+                if (knownSignals[7] != null)
+                {
+                    if (Union(knownSignals[7], signalInformation).Length == 3)
+                    {
+                        knownSignals[3] = signalInformation;
+                        return true;
+                    }
+                }
+                // If there are two similarities between 4 and our signal it's 2
+                if (knownSignals[4] != null)
+                {
+                    if (Union(knownSignals[4], signalInformation).Length == 2)
+                    {
+                        knownSignals[2] = signalInformation;
+                        return true;
+                    }
+                    if (Union(knownSignals[4], signalInformation).Length == 3)
+                    {
+                        knownSignals[5] = signalInformation;
+                        return true;
+                    }
+                }
+                break;
+            // Digits 0,6,9 all have 6 segments
+            case 6:
+                // If there is one similarity between 1 and our signal it's 6
+                if (knownSignals[1] != null)
+                {
+                    if (Union(knownSignals[1], signalInformation).Length == 1)
+                    {
+                        knownSignals[6] = signalInformation;
+                        return true;
+                    }
+                }
+                // If there are 2 similarities between 7 and our signal it's 6
+                if (knownSignals[7] != null)
+                {
+                    if (Union(knownSignals[7], signalInformation).Length == 2)
+                    {
+                        knownSignals[6] = signalInformation;
+                        return true;
+                    }
+                }
+                // If there are two similarities between 4 and our signal it's 2
+                if (knownSignals[4] != null)
+                {
+                    if (Union(knownSignals[4], signalInformation).Length == 4)
+                    {
+                        knownSignals[9] = signalInformation;
+                        return true;
+                    }
+                }
+
+                if (knownSignals[0] != null && knownSignals[6] != null)
+                {
+                    knownSignals[9] = signalInformation;
+                    return true;
+                }
+                if (knownSignals[0] != null && knownSignals[9] != null)
+                {
+                    knownSignals[6] = signalInformation;
+                    return true;
+                }
+                if (knownSignals[9] != null && knownSignals[6] != null)
+                {
+                    knownSignals[0] = signalInformation;
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Create a decode map by looking at all the known signals and deducting the segments
+    /// </summary>
+    /// <param name="knownSignals">IReadOnlyList with known signals</param>
+    /// <returns>char array for the decoding</returns>
+    private static char[] CreateDecodeMap(IReadOnlyList<string> knownSignals)
+    {
+        // Make mapping
+        var decodeMap = new char[7];
+
+
+        // When you look what 7 has more than 1, you know what the encoded 'a' segment is
+        var aEncoded = Difference(knownSignals[7], knownSignals[1])[0];
+        decodeMap[(byte)aEncoded - (byte)'a'] = 'a';
+
+        // When you look what 9 has more than 3, you know what the encoded 'b' segment is
+        var bEncoded = Difference(knownSignals[9], knownSignals[3])[0];
+        decodeMap[(byte)bEncoded - (byte)'a'] = 'b';
+
+        // When you look what 8 has more than 6, you know what the encoded 'c' segment is
+        var cEncoded = Difference(knownSignals[8], knownSignals[6])[0];
+        decodeMap[(byte)cEncoded - (byte)'a'] = 'c';
+
+        // When you look what 8 has more than 0, you know what the encoded 'd' segment is
+        var dEncoded = Difference(knownSignals[8], knownSignals[0])[0];
+        decodeMap[(byte)dEncoded - (byte)'a'] = 'd';
+
+        // When you look what 8 has more than 9, you know what the encoded 'e' segment is
+        var eEncoded = Difference(knownSignals[8], knownSignals[9])[0];
+        decodeMap[(byte)eEncoded - (byte)'a'] = 'e';
+
+        // When you look what 8 (minus segment b) has more than 3, you know what the encoded 'f' segment is
+        var fEncoded = Difference(Remove(knownSignals[8], bEncoded), knownSignals[2])[0];
+        decodeMap[(byte)fEncoded - (byte)'a'] = 'f';
+
+        // When you look what 9 (minus segment a) has more than 4, you know what the encoded 'g' segment is
+        var gEncoded = Difference(Remove(knownSignals[9], aEncoded), knownSignals[4])[0];
+        decodeMap[(byte)gEncoded - (byte)'a'] = 'g';
+
+        return decodeMap;
+    }
+
+    /// <summary>
     /// Decode the supplied segments to normal values
     /// </summary>
-    /// <param name="encodedSegments"></param>
+    /// <param name="encodedSegments">string</param>
     /// <param name="decodeMap">Map to decode</param>
     /// <returns>Decoded and normalized segment information</returns>
-    private static string DecodePartTwo(string encodedSegments, char[] decodeMap)
+    private static string DecodeSegments(string encodedSegments, IReadOnlyList<char> decodeMap)
     {
         var aCharArray = encodedSegments.ToCharArray();
         for (int i = 0; i < aCharArray.Length; i++)
---
+++ AdventOfCode/Solutions/Day09.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode.Solutions;
+
+public class Day09 : AdventOfCodeBase
+{
+    
+    public Day09()
+    {
+        Initialize(this.InputExampleFilename);
+    }
+
+    private void Initialize(string path)
+    {
+        Assert.True(File.Exists(path));
+    }
+
+    public override string AnswerPartOne()
+    {
+        var answer = 0;
+ 
+        return $"Answer 1: {answer}";
+    }
+}
